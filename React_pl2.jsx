ccdb67fb2fc8ac4838cb397962f461a39a73c990
user <user@macbook-pro.lan>
2017-08-17 15:36:18 -0700

    Plural Site Project 2

---
+++ React_pl2.jsx
@@ -0,0 +1,62 @@
+const Card = (props) => {
+	return(
+		<div style={{margin: 'len'}}>
+      <img width = '75' src={props.avatar_url}/>
+      <div style={{display: 'Inline-block', marginLeft:10}}>
+       <div style={{fontSize: '16', fontWeight: 'bold'}}>{props.name}</div>
+       <div>{props.company}</div>
+      </div>
+    </div>  
+  );
+};
+
+
+const Cardlst = (props) => {
+	return (
+  	<div>
+      {props.cards.map(card => <Card key={card.id} {...card} />)}
+    </div>
+  );
+};
+
+class Form extends React.Component {
+	state = { userName: ''}
+	handleSubmit = (event) =>{
+  	event.preventDefault();
+    	axios.get(`https://api.github.com/users/${this.state.userName}`)
+    .then(resp => {
+    	this.props.onSubmit(resp.data);
+      this.setState({ userName: ''});
+    });
+  };
+ 	render(){
+ 		return(
+  		<form onSubmit={this.handleSubmit}>
+        <input type="text" 
+        value={this.state.userName}
+        onChange={(event) => this.setState({userName: event.target.value})}
+        placeholder="Github username" required />
+        <button type="submit">Add Card</button>
+      </form>
+    );
+   }
+}
+class App extends React.Component {
+ 	state = {
+  cards: []
+  };
+  addNewCard = (cardInfor) =>{
+  	this.setState(prevState =>{
+    	cards: prevState.cards.concat(cardInfo)
+    });
+  }; 
+	render(){
+  	return(
+    	<div>
+        <Form onSubmit={this.addNewCard} />
+        <Cardlst cards={this.state.cards}/>
+      </div>
+    )
+  }
+}
+ReactDOM.render(<App />, mountNode);
